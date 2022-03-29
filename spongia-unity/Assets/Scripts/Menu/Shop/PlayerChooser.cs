82c663584404cba6421106aa41897aacc4e10dba
Carrrotka <90919351+Carrrotka@users.noreply.github.com>
2022-03-29 20:16:31 +0200

    Preview and unlocked function

--- spongia-unity/Assets/Scripts/Menu/Shop/PlayerChooser.cs
+++ spongia-unity/Assets/Scripts/Menu/Shop/PlayerChooser.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
-
+using Newtonsoft.Json;
 public class PlayerChooser : MonoBehaviour
 {
     public List<Skins> skinList;
     public GameObject Scroll;
-    public List<GameObject> Children;
+    
     public GameObject Template;
     // Start is called before the first frame update
     void Start()
@@ -24,29 +25,65 @@ public class PlayerChooser : MonoBehaviour
 
     public void ChoosePlayer()
     {
-
-
-        int a = 0;
-        GameObject SkinSelect;
-        foreach(Skins file in skinList)
+        print(Application.persistentDataPath+"/skinInfo.json");
+        Dictionary<string, Dictionary<string, bool>> skinDetails2 = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, bool>>>(File.ReadAllText(Application.persistentDataPath+"/skinInfo.json"));
+        Dictionary<string, bool> skinDetails = skinDetails2["player_skins"];
+        string name = "none";
+        if (Scroll.transform.childCount > 0)
         {
-            
-            
-            SkinSelect = Instantiate(Template, new Vector2(0, 0), Quaternion.identity);
-            SkinSelect.transform.SetParent(Scroll.transform);
-            foreach(Transform child in SkinSelect.transform)
-            {
-                Children.Add(child.gameObject);
-            }
-
-            Children[0].GetComponent<Button>().image.sprite = file.skin;
-            Children[1].GetComponent<Text>().text = file.cost.ToString();
-            Children[2].GetComponent<Text>().text = file.name;
-            SkinSelect.transform.localScale = new Vector2(1, 1);
-            SkinSelect.transform.localPosition = new Vector3(0, 0 + a*350, 0);
-            a++;
-            Children.Clear();
+            name = Scroll.transform.GetChild(0).name;
         }
         
+        if (name == "PlayerSkin(Clone)")
+        {
+            foreach (Transform child in Scroll.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+        else
+        {
+
+
+            int a = 0;
+            GameObject SkinSelect;
+            foreach (Skins file in skinList)
+            {
+
+
+                SkinSelect = Instantiate(Template, new Vector2(0, 0), Quaternion.identity);
+                SkinSelect.transform.SetParent(Scroll.transform);
+                foreach (Transform child in SkinSelect.transform)
+                {
+                    if (child.name == "Button") child.GetComponent<Button>().image.sprite = file.skin;
+                    if (child.name == "Cost") child.GetComponent<Text>().text = file.cost.ToString();
+                    if (child.name == "Name") child.GetComponent<Text>().text = file.name;
+
+                }
+                
+                if (skinDetails[file.name])
+                {
+                    foreach (Transform child in SkinSelect.transform)
+                    {
+                        if (child.name == "Button")
+                        {
+                            
+                            Image _image = child.GetComponent<Button>().GetComponent<Image>();
+                            Color32 newColor = _image.color;
+                            newColor.a = 255;
+                            _image.color = newColor;
+                        }
+                            
+                        
+
+                    }
+                }
+                SkinSelect.transform.localScale = new Vector2(1, 1);
+                SkinSelect.transform.localPosition = new Vector3(0, 0 + a * 350, 0);
+                a++;
+
+            }
+
+        }
     }
 }
